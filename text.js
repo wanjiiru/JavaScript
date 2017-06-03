8eae6e48b03dc15e95cc57449200625d2e87e72c
Wanjiiru <liz240@live.com>
2017-06-03 14:25:35 +0300

    changes

--- text.js
+++ text.js
@@ -1,15 +1,53 @@
 'use strict'
 function Person(first, last, age, email, gender){
-	this.firstName=first;
-	this.lastName=last;
-	this.age=age;
-	this.email=email;
-	this.gender=gender;
+	this.firstName = first;
+	this.lastName = last;
+	this.age = age;
+	this.email = email;
+	this.gender = gender;
 
 }
-Person.prototype.name=function(){
-	return this.firstName + " "+this.lastName };
-var myCousin =new Person("liz","wanjiiru","23","liz240@live","female");
 
-document.getElementById("sample").innerHTML="My cousin "+ myCousin.firstName +"is" +" "+myCousin.age +"years";
+Person.prototype.name = function(){
+	return this.firstName + " "+this.lastName 
+};
+
+var myCousin = new Person("liz","wanjiiru","23","liz240@live","female");
+
+document.getElementById("sample").innerHTML = "My cousin " + " " + myCousin.firstName + " " + "is" + " " + myCousin.age + " " + "years";
+
+
+//closures
+var secretPassword = (function(guess) {
+  var password = 'lizzie';
+
+  return function(guess) {
+    return guess === password;
+  }
+})();
+
+var guessPassword = function (value,event) {
+
+  if(event.keyCode==13) {
+    
+    var guessed;
+
+    var status = secretPassword(value);
+    
+    if(status) 
+    {
+      guessed = 'Yah!, you guessed right'
+    }
+    else 
+    {
+       guessed = 'Whoops, poor guess';
+    }
+
+    document.getElementById("guess").innerHTML = guessed;
+  }
+  
+}
+
+var passwordGame = secretPassword('lizzie');
+
 
